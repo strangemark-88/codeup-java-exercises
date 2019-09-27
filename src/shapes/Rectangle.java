5219f1e95a7b56a68836240e22c50054f8b460d2
Mark Strange <strange.mark88@gmail.com>
2019-09-27 12:03:39 -0500

    complete square exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,20 +2,35 @@ package shapes;
 
 public class Rectangle {
 
-    protected int length;
-    protected int width;
+    protected double length;
+    protected double width;
 
-    public Rectangle(int length, int width) {
+    public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
 
-    public int getArea(){
+    public double getArea(){
         return this.length * this.width;
     };
 
-    public int getPerimeter(){
+    public double getPerimeter(){
         return (this.length * 2) + (this.width * 2);
     }
 
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -3,18 +3,21 @@ package shapes;
 public class Square extends Rectangle{
 
 
-    public Square(int side) {
+    public Square(double side) {
         super(side, side);
     }
 
-
-    public int getPerimeter() {
-        return 4 * this.length;
+    @Override
+    public double getPerimeter() {
+        System.out.println("square over-ride");
+        return 4 * super.length;
 
     }
 
-    public int getArea() {
-        return this.length * this.length;
+    @Override
+    public double getArea() {
+        System.out.println("square over-ride");
+        return Math.pow(super.length, 2);
     }
 
 }
